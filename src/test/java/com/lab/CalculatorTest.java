5bcbee74d4f68cd3657386b79d1945b149c8ec59
unknown <ntrieung@gmail.com>
2026-05-05 15:51:40 +0700

    test CI

--- src/test/java/com/lab/CalculatorTest.java
+++ src/test/java/com/lab/CalculatorTest.java
@@ -8,7 +8,7 @@ class CalculatorTest {
     Calculator calc = new Calculator();
     @Test void testAdd() { assertEquals(5, calc.add(2, 3)); }
     @Test void testMultiply() { assertEquals(20, calc.multiply(4, 5)); }
-    @Test void falseTest() { assertEquals(99, calc.add(2, 3)); a}
+    @Test void falseTest() { assertEquals(99, calc.add(2, 3)); }
     @Test void testDivide() { assertEquals(2.5, calc.divide(5, 2), 0.001); }
     @Test void testDivideByZero() {
         assertThrows(IllegalArgumentException.class, () -> calc.divide(10, 0));
